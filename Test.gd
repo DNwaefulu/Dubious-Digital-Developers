c5ce926db91d9194fbb2f99874d37bdd21365057
PersonalityMain <sonicalefan37@gmail.com>
2023-09-14 00:30:01 -0500

    Rough Humble Beginnings
    Just a simple template to get started on, ONE CONTROLLER IS CONTROLLING BOTH AND THEN THE OTHER ONE IS FINE, if someone could fix that that'd be great. I'm just trying to get 2 controllers working at the same time right now.

---
+++ Test.gd
@@ -0,0 +1,19 @@
+extends ColorRect
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print(Input.get_connected_joypads())
+	print(Input.get_joy_name(0))
+	print(Input.get_joy_name(1))
+	 # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
