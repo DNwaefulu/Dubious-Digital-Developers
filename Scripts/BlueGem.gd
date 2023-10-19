e7a055d82bd9ed0993895e71580039564587d0cc
smb6088 <smb6088@mavs.uta.edu>
2023-10-18 23:36:44 -0500

    Revised Main menu and added settings

---
+++ Scripts/BlueGem.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+onready var anim = $Sprite/AnimationPlayer
+onready var collision = $Area2D/CollisionShape2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	anim.play("rotate")
+
+
+func _on_Area2D_body_entered(body: Node) -> void:
+	queue_free()
