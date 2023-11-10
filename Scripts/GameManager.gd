a8b7f8d66579a17f27fd10e9168f3e6fbe70fb99
Gavgeo9158 <99772456+Gavgeo9158@users.noreply.github.com>
2023-11-09 22:08:21 -0600

    Added gem Counter
    Added the gem counter but encountered a bug where player 2 could double collect gems

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 signal LiveCount(playerLives)
+signal GemCount(globalGemCount)
 signal GameOver()
 
 #this is where the players will spawn back after they die
@@ -27,14 +28,16 @@ func _ready() -> void:
 #so this one gets called when the gem called gem1 gets collected, and it increases the gemCount by 1
 func _on_Gem1_body_entered(_body: Node) -> void:
   globalGemCount += 1
+  emit_signal("GemCount", globalGemCount)
 
 #same situation here, when gem2 gets colllected then we increase gemcount by 1 again 
 func _on_Gem2_body_entered(_body: Node) -> void:
   globalGemCount += 1
+  emit_signal("GemCount", globalGemCount)
 
 func _on_Gem_body_entered(_body: Node) -> void:
   globalGemCount += 1
-
+  emit_signal("GemCount", globalGemCount)
 
 #Whenever the goalsbody is intersected with a KinematicBody2D (a player) then it will check if enough gems have been collected, 
 #if < 3 gems are collected then they won't move on, if they do then they will go to the next level
