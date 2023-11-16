beac9f3413664e8fff3a747d558feb45b1d0f58e
Gavgeo9158 <gavin.george@mavs.uta.edu>
2023-11-16 12:35:28 -0600

    fixed gameOver not disappering after death
    moved pause node above gameOver which fixed the issue. also disabled script that made esc on the levels to go back to the main menu

--- Scripts/Level2Map.gd
+++ Scripts/Level2Map.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-func _process(_delta):
-    if Input.is_action_just_pressed("ui_cancel"):
+#func _process(_delta):
+    #if Input.is_action_just_pressed("ui_cancel"):
     # warning-ignore:return_value_discarded
-        if get_tree().change_scene("res://Scenes/MainMenu.tscn") != OK:
-            print("Error!")
-        queue_free()
+        #if get_tree().change_scene("res://Scenes/MainMenu.tscn") != OK:
+            #print("Error!")
+        #queue_free()
 
 func _on_TextureButton_pressed():
     # warning-ignore:return_value_discarded
