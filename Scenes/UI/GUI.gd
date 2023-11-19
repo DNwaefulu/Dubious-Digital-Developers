7e88678982860cc2b4a6742c9b22a0fda2c4d6c2
Gavgeo9158 <99772456+Gavgeo9158@users.noreply.github.com>
2023-11-19 14:43:47 -0600

    merge 1/2
    first half of merges to fix level1, still need to fix

---
+++ Scenes/UI/GUI.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+signal GemCount()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
+
+
+func _on_Gem_body_entered(body):
+    emit_signal("GemCount")
--- Scripts/Goal.gd
+++ Scripts/Goal.gd
@@ -2,6 +2,10 @@ extends Area2D
 
 const LevelSelect = preload("res://Scenes/Levelselect.tscn")
 
+
+var playerinGoal = 0
+var gemCount = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     pass # Replace with function body.
@@ -16,3 +20,26 @@ func _on_GameManager_LevelOver():
 func _on_Explosionm_animation_finished():
     # warning-ignore:return_value_discarded
     get_tree().change_scene_to(LevelSelect)
+
+
+func _on_Goal_body_entered(body):
+    if body.is_in_group("Climber"):
+        playerinGoal += 1
+        print("P")
+        print(playerinGoal)
+    if playerinGoal == 2:
+        print("TPIG")
+        if gemCount == 3:
+            print("TGC")
+            $Fireworks/Explosionm.show()
+            $Fireworks/Explosionm.play("default")
+            $Fireworks/FireworkPlayer.play()
+
+
+func _on_GUI_GemCount():
+    gemCount += 1
+
+
+func _on_Goal_body_exited(body):
+    if body.is_in_group("Climber"):
+        playerinGoal -= 1
