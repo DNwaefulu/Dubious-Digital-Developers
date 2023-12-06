3f2378f7e090ae49d02b8a4f6814b7401901708f
Siddharth Bhagvagar <smb6088@mavs.uta.edu>
2023-12-05 23:08:46 -0600

    level-2 complete and checkpoint added

--- LevelMaps/Level2Map.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _enter_tree():
-    if Checkpoint.last_position_first:
-        $Player1.global_position = Checkpoint.last_position_first
-    elif Checkpoint.last_position_second:
-        $Player2.global_position = Checkpoint.last_position_second
--- Scripts/Level2Map.gd
+++ Scripts/Level2Map.gd
@@ -1,14 +1,7 @@
 extends Node2D
 
-#func _process(_delta):
-    #if Input.is_action_just_pressed("ui_cancel"):
-    # warning-ignore:return_value_discarded
-        #if get_tree().change_scene("res://Scenes/MainMenu.tscn") != OK:
-            #print("Error!")
-        #queue_free()
-
-func _on_TextureButton_pressed():
-    # warning-ignore:return_value_discarded
-    if get_tree().change_scene("res://Scenes/MainMenu.tscn") != OK:
-        print("Error!")
-    queue_free()
+func _enter_tree():
+    if Checkpoint.last_position_first:
+        $Player1.global_position = Checkpoint.last_position_first
+    elif Checkpoint.last_position_second:
+        $Player2.global_position = Checkpoint.last_position_second
