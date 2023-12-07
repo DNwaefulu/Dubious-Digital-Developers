bf94256d5a98343b3f0477537975ce253d9e4326
Gavgeo9158 <99772456+Gavgeo9158@users.noreply.github.com>
2023-12-07 16:10:26 -0600

    fixes

--- LevelMaps/LavaTile.gd
+++ LevelMaps/LavaTile.gd
@@ -12,15 +12,15 @@ var original_position: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	original_position = position
-	$Timer.start()
+    original_position = position
+    $Timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if position.y < max_height:
-		position.y += delta * rising_speed
+    if position.y < max_height:
+        position.y += delta * rising_speed
 
 func _on_Timer_timeout():
-	if position.y < max_height:
-		position.y += rising_speed
+    if position.y < max_height:
+        position.y += rising_speed
