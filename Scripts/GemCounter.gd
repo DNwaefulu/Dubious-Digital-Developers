60f15593b5d9ee61514e0e1866451dcbab6b3d58
Gavgeo9158 <gavin.george@mavs.uta.edu>
2023-12-07 16:36:00 -0600

    l3 tile stretch

--- Scripts/GemCounter.gd
+++ Scripts/GemCounter.gd
@@ -3,9 +3,9 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$GemSprite1/AnimationPlayer.play("rotate")
-	$GemSprite2/AnimationPlayer.play("rotate")
-	$GemSprite3/AnimationPlayer.play("rotate")
+    $GemSprite1/AnimationPlayer.play("rotate")
+    $GemSprite2/AnimationPlayer.play("rotate")
+    $GemSprite3/AnimationPlayer.play("rotate")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,10 +14,10 @@ func _ready():
 
 
 func _on_GameManager_GemCount(globalGemCount):
-	print(globalGemCount)
-	if globalGemCount == 1:
-		$GemSprite1.show()
-	if globalGemCount == 2:
-		$GemSprite2.show()
-	if globalGemCount == 3:
-		$GemSprite3.show()
+    print(globalGemCount)
+    if globalGemCount == 1:
+        $GemSprite1.show()
+    if globalGemCount == 2:
+        $GemSprite2.show()
+    if globalGemCount == 3:
+        $GemSprite3.show()
